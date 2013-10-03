025a635e9a781b6541b6fd5b48a902ad65252faa
Diego Didona <didona@gsd.inesc-id.pt>
2013-10-03 15:38:28 +0100

    Adding fakeOracle to help in testing

---
+++ src/main/java/fake/FakeOracle.java
@@ -0,0 +1,77 @@
+package fake;
+
+import eu.cloudtm.autonomicManager.commons.ForecastParam;
+import eu.cloudtm.autonomicManager.commons.Param;
+import eu.cloudtm.autonomicManager.commons.ReplicationProtocol;
+import eu.cloudtm.autonomicManager.oracles.InputOracle;
+import eu.cloudtm.autonomicManager.oracles.Oracle;
+import eu.cloudtm.autonomicManager.oracles.OutputOracle;
+import eu.cloudtm.autonomicManager.oracles.exceptions.OracleException;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+/**
+ * // TODO: Document this
+ *
+ * @author diego
+ * @since 4.0
+ */
+public class FakeOracle implements Oracle {
+
+   private final static double WIN_T = 1D;
+   private final static double LOSE_T = 0D;
+
+   private final static double WIN_R = 1D;
+   private final static double LOSE_R = 100D;
+
+   private final static double WIN_A = 0D;
+   private final static double LOSE_A = 1D;
+
+   private final static Log log = LogFactory.getLog(FakeOracle.class);
+
+   @Override
+   public OutputOracle forecast(InputOracle inputOracle) throws OracleException {
+      final boolean t = log.isTraceEnabled();
+      ReplicationProtocol rp = winnerRP(inputOracle);
+      if (inputOracle.getForecastParam(ForecastParam.ReplicationProtocol).equals(rp))
+         return buildWinnerFakeOutput(inputOracle);
+      return buildLoserFakeOutput(inputOracle);
+   }
+
+
+   private FakeOutputOracle buildWinnerFakeOutput(InputOracle i) {
+      double abort[] = buildDoubleArray(WIN_A);
+      double resp[] = buildDoubleArray(WIN_R);
+      double xput[] = buildDoubleArray(WIN_T);
+      return new FakeOutputOracle(xput, resp, abort);
+   }
+
+   private FakeOutputOracle buildLoserFakeOutput(InputOracle i) {
+      double abort[] = buildDoubleArray(LOSE_A);
+      double resp[] = buildDoubleArray(LOSE_R);
+      double xput[] = buildDoubleArray(LOSE_T);
+      return new FakeOutputOracle(xput, resp, abort);
+   }
+
+
+   private ReplicationProtocol winnerRP(InputOracle inputOracle) {
+      double avgPutsPerTx = (Double) inputOracle.getParam(Param.AvgPutsPerWrTransaction);
+      if (avgPutsPerTx < 5) {
+         return ReplicationProtocol.TWOPC;
+      } else if (avgPutsPerTx >= 5 && avgPutsPerTx <= 20) {
+         return ReplicationProtocol.TO;
+      } else {
+         return ReplicationProtocol.PB;
+      }
+   }
+
+
+   private double[] buildDoubleArray(double value) {
+      return new double[]{value, value};
+   }
+
+   private ReplicationProtocol rp(InputOracle input) {
+      return (ReplicationProtocol) input.getForecastParam(ForecastParam.ReplicationProtocol);
+   }
+
+}
---
+++ src/main/java/fake/FakeOutputOracle.java
@@ -0,0 +1,51 @@
+package fake;
+
+import eu.cloudtm.autonomicManager.oracles.OutputOracle;
+
+/**
+ * // TODO: Document this
+ *
+ * @author diego
+ * @since 4.0
+ */
+public class FakeOutputOracle implements OutputOracle {
+   private double[] xput = new double[2];
+   private double[] respT = new double[2];
+   private double[] abort = new double[2];
+
+   public FakeOutputOracle(double[] xput, double[] respT, double[] abort) {
+      this.xput = xput;
+      this.respT = respT;
+      this.abort = abort;
+   }
+
+   @Override
+   public double throughput(int i) {
+      return xput[i];
+   }
+
+   @Override
+   public double abortRate(int i) {
+      return abort[i];
+   }
+
+   @Override
+   public double responseTime(int i) {
+      return respT[i];
+   }
+
+   @Override
+   public double getConfidenceThroughput(int i) {
+      return 1;
+   }
+
+   @Override
+   public double getConfidenceAbortRate(int i) {
+      return 1;
+   }
+
+   @Override
+   public double getConfidenceResponseTime(int i) {
+      return 1;
+   }
+}
