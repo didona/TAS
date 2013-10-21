6942adb70ff25aa4553c8afd9450fb2b64b91aa3
Diego Didona <didona@gsd.inesc-id.pt>
2013-10-21 14:16:42 +0100

    Removing tweaks

--- src/main/java/tasOracle/rr/TasOracle_RR_TO_QUEUE_CPU_CUBIST_NET_MULE.java
+++ src/main/java/tasOracle/rr/TasOracle_RR_TO_QUEUE_CPU_CUBIST_NET_MULE.java
@@ -14,18 +14,5 @@ import tasOracle.common.TasOutputOracle_TO;
  */
 public class TasOracle_RR_TO_QUEUE_CPU_CUBIST_NET_MULE extends TasOracle_RR_TPC_QUEUE_CPU_CUBIST_NET_MULE {
 
-   private final static double DISCOUNT = .1D;
-   private final static double AB = .05D;
 
-   @Override
-   public OutputOracle forecast(InputOracle input) throws OracleException {
-      TasOutputOracle out = (TasOutputOracle) super.forecast(input);
-      double thR = out.throughput(0) * (1.0D - DISCOUNT);
-      double thW = out.throughput(1) * (1.0D - DISCOUNT);
-      double rR = out.responseTime(0) * (1.0D + DISCOUNT);
-      double rW = out.responseTime(1) * (1.0D + DISCOUNT);
-      double aW = out.abortRate(1) * (1.0D - AB);
-
-      return new TasOutputOracle_TO(null, thR, thW, rR, rW, 0, aW);
-   }
 }
